b169e4132099ecb597a1ca20763873cb5f0e9d5e
andresfeliper3 <andresfeliper.2001@gmail.com>
2023-02-01 15:27:57 -0500

    hsl2rgb

--- color/app.js
+++ color/app.js
@@ -167,10 +167,13 @@ function hsv2rgb(H, S, V) {
 }
 
 function hsl2rgb(h, s, l) {
-    let a = s * Math.min(l, 1 - l);
-    let f = (n, k = (n + h / 30) % 12) =>
-        l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
-    return [f(0) * 255, f(8) * 255, f(4) * 255];
+    s /= 100;
+    l /= 100;
+    const k = n => (n + h / 30) % 12;
+    const a = s * Math.min(l, 1 - l);
+    const f = n =>
+        l - a * Math.max(-1, Math.min(k(n) - 3, Math.min(9 - k(n), 1)));
+    return [255 * f(0), 255 * f(8), 255 * f(4)];
 }
 
 
